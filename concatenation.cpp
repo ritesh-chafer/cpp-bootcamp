c5a0b34386c94b07e0bcb8ecff3cfdb6b2a90b76
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-10-13 19:52:32 +0530

    cin

---
+++ concatenation.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using std::cout;
+using std::cin;
+int main()
+{   int slices;
+    cin >> slices;
+    cout << "You Have " << slices << " slices of Pizza."<< std:: endl;
+    return 0;
+}
