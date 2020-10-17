bc0d1f14bac88439faab1ad473008164d086cbeb
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-10-17 21:37:54 +0530

    const enum

---
+++ enum_macro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<float.h>
+using namespace std;
+#define X 10
+
+int main()
+{   //Constant const, macro, and enum
+    const int c = 5;
+    int int_age = 5;
+    
+    enum { y = 100 };
+
+}
