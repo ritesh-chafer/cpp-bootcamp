6c376e9bfa3f6bf7f1602f2dc4c3302b6d6201a6
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-10-17 22:08:50 +0530

    Numeric Functions

---
+++ Numeric_functions.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    cout<<sqrt(25)<<endl;
+    cout<<pow(25,3)<<endl;
+    cout<<remainder(10,3)<<endl;
+    cout<<floor(-1.5)<<endl;
+}
