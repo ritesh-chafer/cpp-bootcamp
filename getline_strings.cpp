8f0e8f387837c6b4fbe15357fecee6f294407341
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-10-19 02:40:03 +0530

    getline()

---
+++ getline_strings.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string greeting;
+    string left_over;
+
+    //getline()
+    getline(cin,greeting);  
+    cout<<greeting<<endl;
+}
